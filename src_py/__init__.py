0e031c1f9e82842b08d3053c3240585e3b3f5c14
Ordo Flammae <ordoflammae@gmail.com>
2020-01-03 10:46:46 -0600

    Add function to read CNF from string.

--- src_py/__init__.py
+++ src_py/__init__.py
@@ -1,1 +1,14 @@
 from pylgl_c import *
+
+
+def read_cnf(cnf):
+    result = []
+    lines = cnf.split('\n')
+
+    for line in lines[1:]:
+        items = line.split()
+
+        if len(items) > 1:
+            result.append(items[:-1])
+
+    return result
